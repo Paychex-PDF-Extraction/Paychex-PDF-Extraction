a7419b94f277030f5e140db48d4cb46ee979a40f
Patrick Kelley <kell3991@fredonia.edu>
2020-04-06 12:57:57 -0400

    Basic db interface, needs queries and comments

---
+++ couchdb_api_inter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MyCouch;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace couchdb_api_inter
+{
+    class MainClass
+    {
+        public static async Task Main(string[] args)
+        {
+            Uri uri = new Uri("http://<username>:<password>@127.0.0.1:5984");
+            String database = "/albums";
+
+            db dbase = new db(uri, database);
+
+            //string docString = "{\"id\": \"002\", \"title\": \"interstellar\", " +
+            //	"\"artist\": \"Incubus\"}";
+
+            var result = await dbase.GetDocById("66a1eac43aa362a96194ec1828000e8b");
+            //var result = await dbase.GetDocByKey("title");
+            Console.WriteLine(result);
+
+            // To Do: functions for reading from and writing documents
+            //      1. return document by key value
+            //      2. add document
+            //      3. update document
+            //      4. delete document
+        }
+    }
+}
---
+++ couchdb_api_inter/db.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using MyCouch;
+
+namespace couchdb_api_inter
+{
+    /* Database object class 
+     * Acts as an intermediary between the couchdb database being called
+     * and the application calling it.    
+     */   
+    public class db
+    {
+        private string dbname;
+        private Uri uri;
+
+        // db constructor
+        public db(Uri uri, string dbname)
+        {
+            this.dbname = dbname;
+            this.uri = uri;
+        }
+
+        // db constructor
+        public db(string uri, string dbname)
+        {
+            Uri s = new Uri(uri);
+            this.dbname = dbname;
+            this.uri = s;
+        }
+
+        // Add document to database
+        public async Task AddDoc(string doc)
+        {
+            using (var client = new MyCouchStore(uri, dbname))
+            {
+
+                var response = await client.StoreAsync(doc);
+                Console.WriteLine(response);
+                client.Dispose();
+            }
+        }
+
+        // search database by "_id" value, NOT "id" value and
+        // return document
+        public async Task<string> GetDocById(string id)
+        {
+            string result;
+            using (var client = new MyCouchStore(uri, dbname))
+            {
+                result = await client.GetByIdAsync(id);
+                client.Dispose();
+            }
+
+            return result;
+        }
+
+        // search database for documents with given key value
+        /*
+        public async Task<string> GetDocByKey(string key, ViewIdentity view)
+        {
+
+            string result;
+            object[] keys = key.Split(',');
+            using (var client = new MyCouchStore(uri, dbname))
+            {
+                result = await client.GetValueByKeysAsync(view, keys, onResult);
+            }
+
+            return result;
+        }
+        */
+    }
+}
