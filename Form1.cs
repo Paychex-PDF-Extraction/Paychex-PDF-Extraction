b16bbfb476a846cbaba465de6d17bcee05f0e383
Adriana Hernandez <61980313+hern9847@users.noreply.github.com>
2020-04-21 19:48:37 -0400

    Create Form1.cs
    Fixed the output to not repeat letters. Still needs work on comprehensible strings.

---
+++ Form1.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iText.Barcodes;
+using iText.Forms;
+using iText.IO;
+using iText.Kernel;
+using iText.Layout;
+using iText.Pdfa;
+using iText.Signatures;
+using iText.StyledXmlParser;
+using iText.Svg;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using IOException = System.IO.IOException;
+using iText.Kernel.Pdf.Canvas.Parser.Data;
+using iText.Kernel.Pdf.Canvas.Parser.Filter;
+
+namespace Paychex_itext
+{
+    
+	public partial class Paychex : Form
+	{
+		public Paychex()
+		{
+			InitializeComponent();
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            this.openFileDialog1.Filter = "PDF Files|*.pdf";
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                txtFilePath.Text = file;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                    
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+           txtResults.Text = PDFread.GetPdfPageText(txtFilePath.Text, 1);
+          
+            
+        }
+    }
+
+    public static class PDFread
+    {
+        public static string GetPdfPageText(string filePath, int pageNum)
+        {
+            using (PdfDocument pdfDocument = new PdfDocument(new PdfReader(filePath)))
+            {
+
+             
+                ITextExtractionStrategy extractionsStrategy = new SimpleTextExtractionStrategy();
+                string pageText = PdfTextExtractor.GetTextFromPage(pdfDocument.GetPage(pageNum),extractionsStrategy);
+                pageText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default,Encoding.UTF8, Encoding.Default.GetBytes(pageText)));
+               
+               
+
+                pdfDocument.Close();
+               
+                return pageText;
+                
+             }
+
+        }
+    }
+}
